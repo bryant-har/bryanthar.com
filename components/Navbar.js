1ea77878800c3b5edf1b4cdb0f3d11fb7f526689
bjh254 <bjh254@cornell.edu>
2023-06-04 00:30:46 -0400

    make navbar transparent

--- components/Navbar.js
+++ components/Navbar.js
@@ -36,7 +36,7 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="relative" color="transparent" elevation={0}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
