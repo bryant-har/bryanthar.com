71afbeeb32c252ccad5951da41c8a987cdea1118
Bryant Har <bjhhar6475@gmail.com>
2023-06-10 00:29:53 -0400

    Flesh out InputMain

--- components/InputMain.js
+++ components/InputMain.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { Paper } from '@mui/material'
 
 export default function Mathinput() {
   return (
@@ -10,8 +11,10 @@ export default function Mathinput() {
         maxWidth: '100%',
       }}
     >
+        <Paper  elevation={2} >
       <TextField fullWidth label="fullWidth" id="fullWidth" />
-    </Box>
+      </Paper>
+      </Box>
   );
 }
 
