e0790563e0384e7bfd0165aa343fb18892618258
bjh254 <bjh254@cornell.edu>
2023-06-11 00:27:02 -0400

    Update icon

---
+++ assets/themes.js
@@ -0,0 +1,34 @@
+export const lightTheme = createTheme({
+  palette: {
+    type: "light",
+    background: {
+      panel: "#fafafa",
+      paper: "#fff",
+      default: "#fafafa",
+    },
+    success: {
+      light: "#81c784",
+      main: "#4caf50",
+      dark: "#388e3c",
+    },
+  },
+  input: {
+    textColor: "black",
+    caretColor: "black",
+    background: "#fff",
+  },
+  pie: {
+    noGames: "rgba(0, 0, 0, 0.12)",
+  },
+  setCard: {
+    purple: "#800080",
+    green: "#008002",
+    red: "#ff0101",
+    background: "#fff",
+  },
+  alarm: red[700],
+  profileTable: {
+    row: "#fff",
+  },
+  setFoundEntry: "rgba(130, 170, 100, 0.15)",
+});
