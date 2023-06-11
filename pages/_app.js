c06c1103464fdb82eb44d92eaa8b14844fbd433c
bjh254 <bjh254@cornell.edu>
2023-06-11 00:27:40 -0400

    Fix Broken import

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar"
 import * as React from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
-import "./styles.css"
+import "../styles/globals.css"
 const a = 2;
 function id(a) {
   return a
