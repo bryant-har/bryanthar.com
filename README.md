2f5260872159f6092306fbbb920f300ebc7c8fb7
bjh254 <bjh254@cornell.edu>
2023-06-16 01:27:09 -0400

    Flesh out mathinput

--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 
 Consider migrating Pages Router to /app when stable
+
+
+### Todo
+#### Critical
+- Implement mp3/mp4 converter into a page
+- implement pdf editor / page reorderer
+
+#### Optional
+- Consider moving mathlive script to _document.js
---
+++ etamac/src/components/MathInput.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Head from 'next/head';
+
+const IndexPage = () => {
+  const mathFieldRef = useRef(null);
+  const [value, setValue] = useState('');
+  const [result, setResult] = useState('');
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://unpkg.com/mathlive?module';
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      const mf = mathFieldRef.current;
+      mf.addEventListener('input', handleInputChange);
+
+      return () => {
+        mf.removeEventListener('input', handleInputChange);
+        document.body.removeChild(script);
+      };
+    };
+  }, []);
+
+  const handleInputChange = () => {
+    const mf = mathFieldRef.current;
+    setValue(mf.getValue());
+
+    try {
+      const evaluation = mf.expression.evaluate();
+      setResult(evaluation.latex);
+    } catch (error) {
+      setResult('');
+    }
+
+    renderMathInElement(document.getElementById('result'));
+  };
+
+  return (
+    <>
+      <Head>
+        <title>MathLive Basic Example</title>
+      </Head>
+      <main>
+        <h2>MathLive Basic Example</h2>
+        <math-field id="mf" virtual-keyboard-mode="manual" ref={mathFieldRef}>
+          f(x)=
+        </math-field>
+        <div id="value">{value}</div>
+        <div id="result" dangerouslySetInnerHTML={{ __html: result }} />
+      </main>
+    </>
+  );
+};
+
+export default IndexPage;
