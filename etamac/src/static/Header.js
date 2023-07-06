6e1c66d5c34f19035d08f8f73af73d3a7a3b2701
bjh254 <bjh254@cornell.edu>
2023-07-06 00:53:44 -0400

    Add Links to header. Todo add

--- etamac/src/static/Header.js
+++ etamac/src/static/Header.js
@@ -1,15 +1,33 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
+
+const links = [
+  { name: "projects", href: "/about" },
+  { name: "writing", href: "/about" },
+];
+
+
+//todo add header page changing...
+
 export default function Header() {
   const [pageTitle, setPageTitle] = useState(null);
+  const linkjs = links.map((obj) => <Link href={obj.href}>{obj.name}</Link>);
+
   return (
     <header
       className="layout-md flex justify-between items-start"
     >
       <h1 className="font-bold text-black text-2xl mb-6">
         <Link href="/about">Bryant Har</Link>
+        {pageTitle ?
+          (<span class="page-title">
+            <span class="text-neutral-400">—</span> {pageTitle}
+          </span>) : null
+        }
+
       </h1>
+      {linkjs}
     </header>
   );
 }
