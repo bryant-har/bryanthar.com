9795956dff4d4a60c92fa4babfc5fb907ed15f28
dreamShepherd <bjhhar6475@gmail.com>
2023-07-10 14:28:00 -0400

    Modify arts

--- etamac/src/pages/about/arts.js
+++ etamac/src/pages/about/arts.js
@@ -12,14 +12,14 @@ export default function Arts() {
           I do <it className=" text-purple-400">~creative~</it> stuff sometimes <span className=" text-sm bg-green-100"> ¯\_(ツ)_/¯ </span>.
         </p>
         <p className="py-4">
-          Here's <em className=" bg-slate-200">quick access</em> to some sheets and poems I've previously played and memorized
-          <it className="hover:blur-none blur-[2px] text-gray-400 text-xs transition-all"> (or am going to. promise...)</it>.
+          Here's quick access to some sheets and poems I've previously played and memorized
+          <it className="hover:blur-none blur-[2px] text-gray-400 text-xs transition-all"> (or am going to...)</it>.
           I play piano and enjoy poetry now and again.
           It's mostly for personal use and access, but feel free to take a gander.
         </p>
         <div className="py-2 grid gap-y-4">
-          <Link className="staticlink py-1 bg-yellow-100 text-xl" href="#sheets"> &nbsp; Music ↓ &nbsp;</Link>
-          <Link className="staticlink py-1 bg-yellow-100 text-xl" href="#poems"> &nbsp; Poetry ↓ &nbsp; </Link>
+          <Link className="staticlink py-1 bg-slate-100 text-xl" href="#sheets"> &nbsp; Sheets Repo ↓ &nbsp;</Link>
+          <Link className="staticlink py-1 bg-slate-100 text-xl" href="#poems"> &nbsp; Poetry ↓ &nbsp; </Link>
         </div >
 
       </section >
