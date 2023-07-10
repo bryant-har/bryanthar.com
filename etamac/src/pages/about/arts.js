5bbd3d74616dbf492f7366caaf75408473265f5b
dreamShepherd <bjhhar6475@gmail.com>
2023-07-10 02:39:25 -0400

    Construct links

--- etamac/src/pages/about/arts.js
+++ etamac/src/pages/about/arts.js
@@ -1,9 +1,32 @@
 import layout from "../../static/Layout";
+import Link from "next/link";
 
-P.getLayout = layout
+Arts.getLayout = layout
 
-export default function P() {
+export default function Arts() {
   return <>
-    {/* todo */}
+    <div className="pb-96">
+      <p>
+        Personal Use Sheet Music:
+
+      </p>
+      <Link className="link" href="#sheets">Sheet Music ↓ </Link>
+      <Link className="link" href="#poems">Memorized Poems ↓ </Link>
+    </div>
+    <p>
+      Under Construction
+
+
+
+
+
+
+
+    </p>
+    <div className="pb-96">
+      <div id="sheets">SECTION 1</div>
+      <div id="arts">SECTION 2</div>
+    </div>
+
   </>
 }
