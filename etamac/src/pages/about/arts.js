8f7255e2d20d1d0a1da458fd72f429408e7b98f3
dreamShepherd <bjhhar6475@gmail.com>
2023-07-10 03:09:53 -0400

    Fix styles add creativity

--- etamac/src/pages/about/arts.js
+++ etamac/src/pages/about/arts.js
@@ -5,28 +5,36 @@ Arts.getLayout = layout
 
 export default function Arts() {
   return <>
-    <div className="pb-96">
+    <body className="staticbody">
+      <div className="pb-96">
+        <p>
+          I enjoy <it className=" text-purple-400">~creative~</it> stuff sometimes I
+          guess <span className=" text-sm bg-neutral-200">¯\_(ツ)_/¯</span>. Here's
+          <em>quick access</em> to some sheet musics and poems I've played and memorized <it className="text-gray-400 text-xs">(or going to)</it>.
+          It's mostly for personal use, but feel free to take a gander.
+        </p>
+        <br />
+        <Link className="staticlink text-neutral-600 bg-yellow-100" href="#sheets">Sheet Music ↓ </Link>
+        <br />
+        <Link className="staticlink bg-yellow-100" href="#poems">Memorized Poems ↓ </Link>
+      </div >
       <p>
-        Personal Use Sheet Music:
+        Under Construction
+
+
+
+
+
+
+
 
       </p>
-      <Link className="staticlink" href="#sheets">Sheet Music ↓ </Link>
-      <Link className="staticlink" href="#poems">Memorized Poems ↓ </Link>
-    </div>
-    <p>
-      Under Construction
-
-
-
-
-
-
-
-    </p>
-    <div className="pb-96">
-      <div id="sheets">SECTION 1</div>
-      <div id="poems">SECTION 2</div>
-    </div>
-
+      <div className="pb-96">
+        <section id="sheets">SECTION 1</section>
+        {/* import sheets component */}
+        <section id="poems">SECTION 2</section>
+      </div>
+    </body>
   </>
+
 }
