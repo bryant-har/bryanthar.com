75f0b96d3a7d68f891275e7a4adaa7d6c7c36557
dreamShepherd <bjhhar6475@gmail.com>
2023-07-11 22:05:22 -0400

    Create Sheets List Page

---
+++ etamac/src/static/Sheets.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import styles from "../static/static.module.css"
+
+
+const links = [
+  { name: "projects", href: "/about/projects" },
+  { name: "writing", href: "/about/writing" },
+  { name: "arts", href: "/about/arts" }
+];
+
+export default function SheetsList() {
+  return
+}
