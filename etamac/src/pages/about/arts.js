d513aca9d8a69e5bd33fbb52eebe49378e7a4b76
dreamShepherd <bjhhar6475@gmail.com>
2023-07-11 22:34:52 -0400

    Fix css issue with hovering not working

--- etamac/src/pages/about/arts.js
+++ etamac/src/pages/about/arts.js
@@ -1,34 +1,35 @@
 import Layout from "../../static/Layout";
-import Link from "next/link";
 import styles from "../../static/static.module.css"
 Arts.getLayout = Layout
 
 export default function Arts() {
   return <>
     <div className="staticbody">
-      <section className="pb-96">
+      <section className="pb-18">
         <h1 className={styles.heading}>Arts</h1>
         <p>
-          I do <it className=" text-purple-400">~creative~</it> stuff sometimes <span className=" text-sm bg-green-100"> ¯\_(ツ)_/¯ </span>.
+          I do <it className=" text-purple-400">~creative~</it> stuff sometimes. <span className=" text-sm bg-green-100"> ¯\_(ツ)_/¯ </span>
         </p>
-        <p className="py-4">
+        <p className="pb-4 pt-2">
           Here's quick access to some sheets and poems I've previously played and memorized
           <it className="hover:blur-none blur-[2px] text-gray-400 text-xs transition-all"> (or am going to...)</it>.
           I play piano and enjoy poetry now and again.
           It's mostly for personal use and access, but feel free to take a gander.
         </p>
         <div className="py-2 grid gap-y-4">
-          <Link className="staticlink py-1 bg-slate-100 text-xl" href="#sheets"> &nbsp; Sheets Repo ↓ &nbsp;</Link>
-          <Link className="staticlink py-1 bg-slate-100 text-xl" href="#poems"> &nbsp; Poetry ↓ &nbsp; </Link>
+          <a className="staticlink py-1 bg-slate-100 text-xl" href="#sheets"> &nbsp; Sheets Repo <b>↓</b> &nbsp;</a>
+          <a className="staticlink py-1 bg-slate-100 text-xl" href="#poems"> &nbsp; Poetry <b>↓</b> &nbsp; </a>
         </div >
 
       </section >
       <section id="sheets">
         <h1 className={styles.heading}>Sheets</h1>
+        <div className="pb-96"></div>
         {/* Import sheets list component */}
       </section>
       <section id="poems">
         <h1 className={styles.heading}>Poetry</h1>
+        <div className="pb-96"></div>
       </section>
     </div >
   </>
