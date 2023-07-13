616bad40ebdb980dbec6c56ea3cd2b655b4ec13c
dreamShepherd <bjhhar6475@gmail.com>
2023-07-12 23:51:04 -0400

    Template the sheets list

--- etamac/src/static/SheetItem.tsx
+++ etamac/src/static/SheetItem.tsx
@@ -2,9 +2,40 @@ import Link from 'next/link';
 import styles from "../static/static.module.css"
 import React from 'react'
 
-export default function SheetsList() {
-  return <div>
+type ListItem = {
+  title: string;
+  date: Date;
+  summary: string;
+  link: string;
+};
 
+function toListItem(item: ListItem) {
 
-  </div >
+}
+
+export default function SheetsList(items: Array<ListItem>) {
+  return <div class="grid gap-y-4">
+    items.map()
+    <a
+      href={item.link}
+      class="block -mx-3 px-3 py-2 hover:bg-neutral-100 transition-colors"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <div class="flex flex-col sm:flex-row sm:items-end mb-1.5">
+        <div class="text-lg text-black">
+          {item.title}
+          <ArrowUpRight size={18} class="inline text-neutral-400" />
+        </div>
+        <div class="sm:ml-auto mb-0.5 text-neutral-500">
+          {formatTime("%B %-d, %Y", item.date)}
+        </div>
+      </div>
+      <div class="text-lg leading-snug font-serif italic">
+        {item.summary}
+      </div>
+    </a>
+    {/each}
+</div>
+
 }
