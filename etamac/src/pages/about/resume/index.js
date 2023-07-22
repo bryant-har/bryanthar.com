b974f4a21b163c271d8ac1b308f106c00a52dee0
dreamShepherd <bjhhar6475@gmail.com>
2023-07-21 23:30:44 -0400

    Finish Coursework

--- etamac/src/pages/about/resume/index.js
+++ etamac/src/pages/about/resume/index.js
@@ -14,17 +14,27 @@ export default function Writing() {
         <h1 className="heading">Certifications</h1>
       </section>
 
-      <section className="layout">
-        <h1 id='coursework' className="heading">Relevant Coursework</h1>
-        <NoLinkList data={data.coursework}> </NoLinkList>
-      </section>
-
       {/* todo */}
       {/* Blog Stuff */}
       {/* Fun Facts */}
       {/* Notes */}
       {/* Classes Taken And Notes For those classes */}
       {/* Make tabs like Geoff */}
+
+
+
+      <a id='coursework'><div className="bg-gray-900 py-12 text-neutral-200 dark">
+        <section className="layout">
+          <h1 className="heading text-white" >Coursework</h1>
+        </section>
+      </div ></a>
+
+      <section className="layout pt-12">
+        <h1 className='heading'> Computer Science </h1>
+        <NoLinkList data={data.coursework}> </NoLinkList>
+        <h1 className='heading pt-12'> Math and Physics </h1>
+        <NoLinkList data={data.physcoursework}> </NoLinkList>
+      </section>
     </Transition >
   </>
 }
