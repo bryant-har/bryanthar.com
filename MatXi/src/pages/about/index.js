8e47f0dcaaa056b3723da4c9647fb4778d043cf9
dreamShepherd <bjhhar6475@gmail.com>
2023-07-27 15:09:21 -0400

    Fix styling, add copyright

--- MatXi/src/pages/about/index.js
+++ MatXi/src/pages/about/index.js
@@ -9,9 +9,9 @@ export default function About() {
     <div className='layout'>
       <p className="text-black text-xl leading-tight font-light mb-8"
       >
-        <span className="g">is a </span> software engineer<span className={"g"}>,</span><br />
-        designer<span className={"g"}>, and</span><br />
-        researcher<span className={"g"}>.</span>
+        <span className="gem">is a </span> software engineer<span className="gem">,</span><br />
+        designer<span className="gem">, and</span><br />
+        researcher<span className="gem">.</span>
       </p>
 
       <section className='pb-8'>
@@ -33,42 +33,42 @@ export default function About() {
         </div>
       </section>
 
-      <div className="">
-        <div className="space-y-5 text-md">
+      <div className="space-y-8">
+        <section className='space-y-4 text-md'>
           <p>
             Hello! I currently study <span className='text-blue-500'>physics</span> and <span className='text-blue-500'>computer science</span> and <span className='text-blue-500'>English</span> at <b>Cornell University</b>.
-            My academic interests involve applying <b>machine learning</b> and <b>robotics</b>, though I try to be a <b>generalist</b>.
+            My academic interests involve reinforcement learning and robotics, though I try to be a generalist.
           </p>
           <p>
             My professional experience involves machine learning and statistical analysis.
             I also enjoy chess, tennis, working out memorizing poetry, and listening to music.
           </p>
-          <section className=' text-sm space-y-4'>
-            <h1 className='heading'><span className='text-teal-600'>#</span> Programming</h1>
-            <p>
-              I'm personally interested in <b>web development</b>, <b>robotics</b>, and <b>user design</b>. Check out my <a className='link text-blue-400' href='https://github.com/dreamShepherd'>github</a>. I use the following tools:
-            </p>
-            <p className='leading-relaxed'>
-              <b>Languages:</b> Python · Java · C/C++ · HTML/CSS · Javascript · Wolfram Language · LaTeX <br />
-              <b>Frameworks:</b> NextJS · ReactJS · Tailwind · Bootstrap · ROS · Tensorflow/Keras · PyTorch<br />
-              <b>Tools/Services:</b>VSCode · Firebase · MongoDB · Heroku · Git/Github
+        </section>
+        <section className='text-sm space-y-4'>
+          <h1 className='heading'><span className='text-teal-600'>#</span> Programming</h1>
+          <p>
+            I'm personally interested in <b>web development</b>, <b>robotics</b>, and <b>user design</b>. Check out my <a className='link text-blue-400' href='https://github.com/dreamShepherd'>github</a>. I use the following tools:
+          </p>
+          <p className='leading-relaxed'>
+            <span className='font-semibold'>Languages:</span> Python · Java · C/C++ · HTML/CSS · Javascript · Wolfram Language · LaTeX <br />
+            <span className='font-semibold'>Frameworks:</span> NextJS · ReactJS · Tailwind · Bootstrap · ROS · Tensorflow/Keras · PyTorch<br />
+            <span className='font-semibold'>Tools/Services:</span>VSCode · Firebase · MongoDB · Heroku · Git/Github
 
-            </p>
-          </section>
-          <section className=' text-sm space-y-4'>
-            <h1 className='heading'> <span className='text-teal-600'>#</span> Research</h1>
-            <p>
-              My current research focus is applying machine learning to the sciences. I'm part of an ongoing research project applying
-              machine learning to optimize <a className='link text-blue-400' href='https://en.wikipedia.org/wiki/Stellarator'>stellarator</a> plasma device parameters.
-            </p>
-            <p>
-              Previously, I've worked at the <a href='https://www.spacecraftresearch.com/' className='link'><em>Space Systems Design Studio</em></a>,
-              studying how we can apply computer vision techniques to map lunar materials and regolith from stereo images. Previously, I worked on
-              quantum decoherence with respect to variational quantum eigensolver computations.
-            </p>
-          </section>
+          </p>
+        </section>
+        <section className=' text-sm space-y-4'>
+          <h1 className='heading'> <span className='text-teal-600'>#</span> Research</h1>
+          <p>
+            My current research focus is applying machine learning to the sciences. I'm part of an ongoing research project applying
+            machine learning to optimize <a className='link text-blue-400' href='https://en.wikipedia.org/wiki/Stellarator'>stellarator</a> plasma devices.
+          </p>
+          <p>
+            Previously, I've worked at the <a href='https://www.spacecraftresearch.com/' className='link'><em>Space Systems Design Studio</em></a>,
+            studying how we can apply computer vision techniques to map lunar materials and regolith from stereo images. Before that, I worked on
+            quantifying decoherence in the variational quantum eigensolver.
+          </p>
+        </section>
 
-        </div>
       </div>
     </div >
   );
--- MatXi/src/static/Layout.js
+++ MatXi/src/static/Layout.js
@@ -1,6 +1,7 @@
 import Header from './Header'
 import React from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
+import { Copyright } from 'lucide-react'
 // FIX EXIT ANIMATIONS.
 
 const layout = ((page, key) => (
@@ -24,6 +25,10 @@ const layout = ((page, key) => (
         <li className='link'>Github: <a href='https://github.com/dreamShepherd'>@DreamShepherd</a></li>
         <li className='link'>Linkedin: <a href='https://www.linkedin.com/in/bryant-har-a260ab186/'>@bryant-har</a></li>
       </ul>
+      <div className='flex place-content-center space-x-0.5 items-center opacity-50 pt-4'>
+        <Copyright size={16} />
+        <p className='text-xs'> 2022 - 2023 Bryant Har | Powered by NextJS</p>
+      </div>
     </section>
   </div >
 ))
