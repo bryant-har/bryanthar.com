8617804d68dc76fa730938cbaf39501e08a55a60
dreamShepherd <bjhhar6475@gmail.com>
2023-07-27 00:38:44 -0400

    Add wait page

--- MatXi/src/pages/index.js
+++ MatXi/src/pages/index.js
@@ -1,13 +1,21 @@
 import InputBar from '../components/InputBar'
 import Script from 'next/script'
-
+import { Construction, HardHat } from 'lucide-react'
+import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <h1 className='bg-center text-center heading'> Under Construction! </h1>
-      {/* <Script src="//unpkg.com/mathlive"/>      
+      <div className='space-y-6 my-36'>
+        <h1 className='bg-center flex items-center justify-center space-x-3 heading'>
+          <Construction />
+          <span>Page Under Construction! </span>
+          <HardHat />
+        </h1>
+        <p className='bg-center text-center link'><Link href='/about'> Click me for the about page </Link></p>
+        {/* <Script src="//unpkg.com/mathlive"/>      
       <math-field/>
       <InputBar/> */}
+      </div>
     </>
   )
 }
