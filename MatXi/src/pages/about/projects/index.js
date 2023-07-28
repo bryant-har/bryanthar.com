e17fa0b219641dba5dcc2e364aefb58cc983182e
dreamShepherd <bjhhar6475@gmail.com>
2023-07-28 12:10:16 -0400

    Template for projects

--- MatXi/src/pages/about/projects/index.js
+++ MatXi/src/pages/about/projects/index.js
@@ -1,20 +1,27 @@
 import { layout } from "../../../static/Layout.js";
-
+import data from './projects.yaml'
+import LinkList from "../../../static/LinkList.js";
 Projects.getLayout = layout
 
 export default function Projects() {
-  return <div className="py-12">
-    <section className="layout">
+  return <div className="pt-12">
+    <section className="pb-12 layout text-lg">
       <h1 className="heading">Projects</h1>
-    </section>
-    {/* /#NDA */}
-    {/* todo */}
-    {/* Stealth Startup */}
-    {/* robot learning */}
-    {/* Use template for projects */}
-    {/* Various demonstrations of algo projects... from cs 4820. For fun! */}
-  </div>
-}
+      <p className="pb-4 pt-2">
+        Here's quick overview of my github.
+      </p>
+    </section >
 
-// Textbook Repo as project as well.
-// Add notes repo
+    <div className="bg-gray-900 py-12 text-neutral-200 dark">
+      <section className="layout">
+        <h1 className="heading text-white" >Table of Contents</h1>
+        <div className='grid grid-cols-2 pl-4 text-red-50'>
+          {data.projects.map((item, index) => <li key={index} > {item.title}</li>)}
+        </div>
+      </section>
+    </div >
+    <section className="layout">
+      <LinkList data={data.projects} />
+    </section>
+  </div >
+}
