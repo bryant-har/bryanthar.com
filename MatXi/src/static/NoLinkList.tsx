45093a03e373f530352d3927944c464fb4b204b5
dreamShepherd <bjhhar6475@gmail.com>
2023-07-28 12:11:16 -0400

    Fix typo

--- MatXi/src/static/NoLinkList.tsx
+++ MatXi/src/static/NoLinkList.tsx
@@ -9,7 +9,7 @@ type ListItem = {
 
 
 
-export default function LinkList({ data }: { data: ListItem[] }) {
+export default function NoLinkList({ data }: { data: ListItem[] }) {
   return <div className="grid gap-y-4">
     {data.map((item, index) =>
       <div key={index} className='block -mx-3 px-3 py-2 hover:bg-neutral-100 transition-colors'
