862d02b948185d99239c7a7dfed84cd3f8c10071
dreamShepherd <bjhhar6475@gmail.com>
2023-07-28 21:51:29 -0400

    add seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -19,8 +19,14 @@ export default function App({ Component, Props }) {
 
   return (
     <>
+      <Seo
+        title="Bryant Har"
+        description="Software engineer, researcher, and designer. Making meaningful systems and interfaces for people."
+      />
       <Head>
-        <title>MatXi</title>
+        <title>Bryant Har</title>
+        <meta name="description" content="Software engineer, researcher, physicist, and designer. Meaningful systems and user interfaces"></meta>
+        <meta name = "description" content = "software projects, operating systems, web development, numerical analysis, music, programming languages, machine learning, and more"></meta>
       </Head>
       {getLayout(<Component {...Props} />, router.asPath)}
     </>
