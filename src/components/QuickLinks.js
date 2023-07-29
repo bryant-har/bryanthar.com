001020721a33c629448afc0f21596428dc73fdab
dreamShepherd <bjhhar6475@gmail.com>
2023-07-28 20:58:13 -0400

    preload images

--- src/components/QuickLinks.js
+++ src/components/QuickLinks.js
@@ -1,15 +1,22 @@
 import { Mail } from "lucide-react";
+import Image from 'next/image'
 import Typewriter from "typewriter-effect";
+import doge from '../../public/static/ico.png'
+import git from '../../public/static/github.png'
+import disc from '../../public/static/discord.png'
+import linkedin from '../../public/static/linkedin.png'
+import chess from '../../public/static/lichess.ico'
+
 
 export default function QuickLinks() {
   return (
     <div className="pb-8">
-      <img
+      <Image
         className="mx-auto rounded-full"
         height="auto"
         width="100"
-        src="./static/ico.png"
-      ></img>
+        src={doge}
+      />
       <div className="heading text-2xl text-center mb-2 text-slate-500">
         <Typewriter
           onInit={(typewriter) => {
@@ -19,16 +26,16 @@ export default function QuickLinks() {
       </div>
       <div className="flex items-center justify-center space-x-1">
         <a href="https://github.com/dreamShepherd">
-          <img width="24" height="auto" src="./static/github.png"></img>
+          <Image width="24" height="auto" src={git}/>
         </a>
         <a href="https://lichess.org/@/DreamShepherd">
-          <img width="24" height="auto" src="./static/lichess.ico"></img>{" "}
+          <Image width="24" height="auto" src={chess}/>{" "}
         </a>
         <a href="https://discordapp.com/users/bryant3435">
-          <img width="24" height="auto" src="./static/discord.png"></img>
+          <Image width="24" height="auto" src={disc}/>
         </a>
         <a href="https://www.linkedin.com/in/bryant-har-a260ab186/">
-          <img width="24" height="auto" src="./static/linkedin.png"></img>
+          <Image width="24" height="auto" src={linkedin}/>
         </a>
         <a href="mailto:bryanthar100@gmail.com">
           <Mail />
