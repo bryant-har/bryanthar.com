066c96aa2cd9fa6c753570b93312a19d9a324acb
dreamShepherd <bjhhar6475@gmail.com>
2023-07-29 23:50:23 -0400

    Fix alignmenet and speed

--- src/components/QuickLinks.js
+++ src/components/QuickLinks.js
@@ -17,10 +17,10 @@ export default function QuickLinks() {
         width="100"
         src={doge}
       />
-      <div className="heading text-2xl pl-1 text-center mb-2 text-slate-500">
+      <div className="heading text-2xl pl-1.5 text-center mb-2 text-slate-500">
         <Typewriter
           options={{
-            delay:100
+            delay:75,
           }}
         
 
