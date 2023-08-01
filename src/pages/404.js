51f97b5a7495b715438cf0dedb276214628452d9
dreamShepherd <bjhhar6475@gmail.com>
2023-07-31 22:03:14 -0400

    change 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 Error.getLayout = (page) => page;
 
 export default function Error() {
-  const [img, setImg] = useState("https://http.cat/404");
+  const [img, setImg] = useState("https://i.pinimg.com/originals/2c/d5/f9/2cd5f95facd32432677a2a7f75391468.jpg");
   const fetchImage = async () => {
     const res = await fetch("https://http.cat/404");
     const imageBlob = await res.blob();
@@ -18,7 +18,7 @@ export default function Error() {
   }, []);
 
   return (
-    <a className = 'h-screen flex justify-center bg-black' href="/">
+    <a className = 'h-screen flex justify-center bg-orange-200' href="/">
           <img src={img} alt="404 Missing Page" />
     </a>
   );
